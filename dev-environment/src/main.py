48d7aa84d0fdfe0ee28539918a124d0ef376cb8b
Krzysztof <malarczyk.krzysztof@gmail.com>
2025-08-21 20:52:33 +0200

    first commit

---
+++ dev-environment/src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+import redis
+
+r = redis.Redis(host="redis", port=6379)
+app = FastAPI()
+
+import debugpy
+
+debugpy.listen(("0.0.0.0", 5678))
+print("Waiting for client to attach...")
+debugpy.wait_for_client()
+
+
+@app.get("/")
+def read_root():
+    pilka =1224443
+
+    return {f"Supa:{pilka}": "World"}
+
+
+@app.get("/hits")
+def read_root():
+    r.set("foo", "bar")
+    r.incr("hits")
+    return {"Number of hits:": r.get("hits"), "foo": r.get("foo")}
+
