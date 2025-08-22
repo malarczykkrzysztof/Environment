b8120daa0adc087bd37dad1e6f585567b6136799
Krzysztof <malarczyk.krzysztof@gmail.com>
2025-08-22 07:56:17 +0200

    Remove debug dependency

--- dev-environment/src/main.py
+++ dev-environment/src/main.py
@@ -5,19 +5,9 @@ import redis
 r = redis.Redis(host="redis", port=6379)
 app = FastAPI()
 
-import debugpy
-
-debugpy.listen(("0.0.0.0", 5678))
-print("Waiting for client to attach...")
-debugpy.wait_for_client()
-
-
 @app.get("/")
 def read_root():
-    pilka =1224443
-
-    return {f"Supa:{pilka}": "World"}
-
+    return {f"Supa:": "World"}
 
 @app.get("/hits")
 def read_root():
