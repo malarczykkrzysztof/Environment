51b81c3bc3b1e92fbce782de628f0d858fe20932
Krzysztof <malarczyk.krzysztof@gmail.com>
2025-08-25 19:49:21 +0200

    Update

---
+++ dev-environment/app/tests/test_my_structure.py
@@ -0,0 +1,30 @@
+import pytest   
+from my_project import my_structure    
+    
+@pytest.mark.parametrize("arr, size_of_window, expected_result",[
+(
+    [1,2,3,4,5,6],
+    3,
+    15
+),
+(
+    [1,2,3,4,5,6],
+    2,
+    11
+),
+(
+    [1,2,3,4,5,6],
+    8,
+    -1
+),
+])
+
+def test_slinding_windows(arr, size_of_window, expected_result):
+       
+    result = my_structure.slinding_window(arr, size_of_window)
+    assert result == expected_result
+
+
+# from app directory
+# python -m pytest  ./tests/test_my_structure.py::test_slinding_windows -vv
+# python -m pytest  -vv
